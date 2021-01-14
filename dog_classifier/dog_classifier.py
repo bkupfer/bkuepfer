203e20e3fde7a10b832f5dfa364449f93117d8db
bkupfer <bdokupferb@gmail.com>
2021-01-14 18:09:48 +0100

    added backend for dog classifier module

---
+++ dog_classifier/dog_classifier.py
@@ -0,0 +1,57 @@
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential
+
+from keras.preprocessing import image
+from tqdm import tqdm
+
+import numpy as np
+
+from .extract_bottleneck_features import extract_Xception
+from .dog_names import DOG_NAMES
+
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224, 3))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+
+class DogClassifier:
+
+    def __init__(self):
+        VGG19_model = Sequential()
+        VGG19_model.add(Conv2D(filters=36, kernel_size=2, input_shape=(7, 7, 2048)))
+        VGG19_model.add(MaxPooling2D(pool_size=2, strides=1, padding="same"))
+        VGG19_model.add(GlobalAveragePooling2D())
+        VGG19_model.add(Dense(266, activation="relu"))
+        VGG19_model.add(Dense(133, activation="softmax"))
+
+        VGG19_model.summary()
+
+        VGG19_model.compile(
+            loss='categorical_crossentropy',
+            optimizer='adam',
+            metrics=['accuracy']
+        )
+
+        VGG19_model.load_weights('dog_classifier/saved_models/weights.best.my_Xception_checkpoint.hdf5')
+        self.VGG19_model = VGG19_model
+
+    def Xception_predict_breed(self, img_path):
+        # note: The name of the model is still `VGG19_model` just because laziness,
+        # but the bottleneck features have been changed.
+        # extract bottleneck features
+        bottleneck_feature = extract_Xception(path_to_tensor(img_path))
+        # obtain predicted vector
+        predicted_vector = self.VGG19_model.predict(bottleneck_feature)
+        # return dog breed that is predicted by the model
+        return DOG_NAMES[np.argmax(predicted_vector)]
--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import date
+from dog_classifier.dog_classifier import DogClassifier
 
 
 # Create your views here.
@@ -7,8 +8,18 @@ def index(request):
     today = date.today()
     born = date(1991, 9, 13)
     my_age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-    cv_link = [
+    cv_link = [  # todo: update link
         "https://www.dropbox.com/s/r21duymhq6pts0p/bk%C3%BCpfer_CV%20%5Bjun-2020%5D.pdf?dl=0",  # 06.2020
         "https://www.dropbox.com/s/8bofssviydqb1vk/%5B20.07%5D%20bkuepfer_CV.pdf?dl=0",         # 07.2020
     ]
     return render(request, "index.html", {"my_age": my_age, "cv_link": cv_link[1]})
+
+
+def dog_classifier(request):
+    image_path = '../kali/photo_2021-01-14_16-46-35.jpg'
+    prediction = ''
+    if image_path:
+        classifier = DogClassifier()
+        prediction = classifier.Xception_predict_breed(image_path)
+        print(prediction)
+    return render(request, "dog_classifier.html", {"prediction": prediction})
