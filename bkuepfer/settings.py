abd33d6936b68cb01e5c4a5d03bbd7000248b2d0
bküpfer <bdokupferb@gmail.com>
2023-01-20 03:08:22 +0100

    /dog?

--- bkuepfer/settings.py
+++ bkuepfer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Comment out the following line and place your railway URL, and your production URL in the array.
 CSRF_TRUSTED_ORIGINS = [
     "https://django-server-production-758a.up.railway.app/",
+    "https://django-server-production-758a.up.railway.app/dog",
 ]
 
 # Application definition
